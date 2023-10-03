79a12e5406c3ca74c32288a92b42bf6d4f1d22ad
cory <cory@docker-builder.host>
2023-10-02 23:50:47 -0500

    update: script

--- ipblock.sh
+++ ipblock.sh
@@ -7,11 +7,10 @@
 # modify to specfy config / blocklist dir
 BLOCKLIST_DIR=/opt/etc/ipblock
 BLOCKLIST_GIT=https://raw.githubusercontent.com/codekow/simple-ipblock/dev
-BLOCKLIST_FILE=/tmp/block.list
+BLOCKLIST_FILE=custom.zone
 
-ZONE_MD5SUM=https://www.ipdeny.com/ipblocks/data/aggregated/MD5SUM
 ZONE_URL=https://www.ipdeny.com/ipblocks/data/aggregated
-ZONE_DENY="cn tw in ru br ar bg cz ca"
+ZONE_DENY="cn tw in ru br ar bg cz ca custom"
 ZONE_ALLOW="us"
 PORTS="22,80,443"
 
@@ -20,14 +19,15 @@ PORTS="22,80,443"
 SCRIPT=$0
 LOG_PREFIX="BLOCKED "
 IPSET_MAX=65535
-DROP_TARGET=BLOCK-DROP
+DROP_TARGET=BLOCK
 PREFIX=block-
+JUMP="PREROUTING -i eth0 -p tcp -m multiport --dports ${PORTS} -j ${DROP_TARGET}"
+
 TMP_DIR=$(mktemp -d -t ipblock.XXXX)
 
 send_log(){
   echo "$@" | logger -s -t "$SCRIPT"
 }
-
 
 setup_dir(){
   mkdir -p ${BLOCKLIST_DIR} > /dev/null 2>&1 || BLOCKLIST_DIR="./ipblock" 
@@ -47,9 +47,9 @@ setup_dir(){
 
   [ ! -s ${BLOCKLIST_DENY} ] \
     && curl -sL "${BLOCKLIST_GIT}/cfg/$(basename ${BLOCKLIST_DENY})" -o ${BLOCKLIST_DENY}
-}
 
-cd "${TMP_DIR}"
+  cd "${TMP_DIR}"
+}
 
 check_root(){
   if [ "$(id -u)" != "0" ]; then
@@ -96,66 +96,94 @@ init_ipset(){
   esac
 }
 
-get_contries(){
-
-  curl -sL "${ZONE_MD5SUM}" -o md5sum
-
-  for country in ${ZONE_DENY} ${ZONE_ALLOW}
-  do
-    curl -sL "${ZONE_URL}/${country}-aggregated.zone" -O
-  done
-
-  md5sum -c md5sum --ignore-missing
-}
-
-init(){
-  
-  setup_dir
-  get_contries
-
-  exit 0
-
-  check_root
-  init_ipset
-
-  create_drop_chain "${DROP_TARGET}"
-  create_chain BLOCKED
-  create_jump BLOCKED
-}
-
 create_jump(){
   CHAIN_NAME=$1
-  JUMP="PREROUTING -i eth0 -p tcp -m multiport --dports ${PORTS} -j ${CHAIN_NAME}"
 
-  iptables -t raw -D "${JUMP}"
-  iptables -t raw -I "${JUMP}"
-
+  iptables -t raw -D ${JUMP}
+  iptables -t raw -I ${JUMP}
 }
 
 create_chain(){
   CHAIN_NAME=$1
 
-  # setup logging drop chain
   iptables -t raw -N "${CHAIN_NAME}"
   iptables -t raw -F "${CHAIN_NAME}"
+  iptables -t raw -A "${CHAIN_NAME}" -j DROP
 }
 
-create_drop_chain(){
-  CHAIN_NAME=$1
+# create_drop_chain(){
+#   CHAIN_NAME=$1
 
-  create_chain "${CHAIN_NAME}"
+#   create_chain "${CHAIN_NAME}"
 
-  iptables -t raw -A "${CHAIN_NAME}" \
-    -m limit \
-    --limit 1/min \
-    -j LOG \
-    --log-prefix "${LOG_PREFIX}" \
-    --log-tcp-sequence \
-    --log-tcp-options \
-    --log-ip-options
+#   iptables -t raw -A "${CHAIN_NAME}" \
+#     -m limit \
+#     --limit 1/min \
+#     -j LOG \
+#     --log-prefix "${LOG_PREFIX}" \
+#     --log-tcp-sequence \
+#     --log-tcp-options \
+#     --log-ip-options
 
-  iptables -t raw -A "${CHAIN_NAME}" \
-	 -j DROP
+#   iptables -t raw -A "${CHAIN_NAME}" -j DROP
+# }
+
+get_countries(){
+
+  for country in  ${ZONE_ALLOW} ${ZONE_DENY}
+  do
+    curl -sL "${ZONE_URL}/${country}-aggregated.zone" -O
+  done
+
+  for file in *.zone
+  do
+    name=${file%-aggregated.zone}
+    echo "name: $name"
+    echo "file: $file"
+
+    ipset destroy $name.zone
+    ipset create $name.zone hash:net
+
+    for IP in $(cat $file)
+    do
+      ipset add $name.zone $IP
+    done
+
+    if echo $name | grep -q ${ZONE_ALLOW}; then
+      iptables -t raw -I ${DROP_TARGET} -m set --match-set $name.zone src -j RETURN
+    else
+      iptables -t raw -I ${DROP_TARGET} -m set --match-set $name.zone src -j DROP
+    fi
+
+  done
+
+  # curl -sL "${ZONE_URL}/MD5SUM" -o md5sum
+  # md5sum -c md5sum 2>/dev/null | grep OK
+}
+
+get_lists(){
+  [ -t 1 ] && send_log "get list"
+
+  ( (while read -r url
+  do
+    nice -n 15 curl "$url" -sL
+  done <${BLOCKLIST_URLS} ); [ -s ${BLOCKLIST_DENY} ] && cat ${BLOCKLIST_DENY}) | \
+    nice -n 15 sed -n "s/\r//;s/#.*$//;/^$/d;/^[0-9,\.,\/]*$/p" | \
+    nice -n 15 grep -vf ${BLOCKLIST_ALLOW} | \
+    nice -n 15 awk '!a[$0]++' > ${BLOCKLIST_FILE}
+}
+
+init(){
+  
+  setup_dir
+
+  check_root
+  init_ipset
+  load_ipset
+
+  # create_drop_chain "${DROP_TARGET}"
+  create_chain "${DROP_TARGET}"
+  create_jump "${DROP_TARGET}"
 }
 
 ipset_all_cidr(){
@@ -170,7 +198,10 @@ ipset_all_cidr(){
   (echo "${CREATE} ${PREFIX}.tmp ${NETHASH}"
   sed -n "/\//s/^/$ADD ${PREFIX}.tmp /p" ${IPSET_FILE}
   echo "COMMIT") | nice -n 15 ipset ${RESTORE} && ipset ${SWAP} ${PREFIX}.tmp ${IPSET_NAME}
-  iptables -t raw -nL PREROUTING | grep -q ${IPSET_NAME} || iptables -t raw -I PREROUTING -m set ${MATCH_SET} ${IPSET_NAME} src -j ${CHAIN_TARGET}
+  iptables -t raw -nL ${DROP_TARGET} | grep -q ${IPSET_NAME} || \
+    iptables -t raw -I ${DROP_TARGET} \
+    -m set ${MATCH_SET} ${IPSET_NAME} src \
+    -j DROP
 }
 
 ipset_all_ip(){
@@ -191,14 +222,17 @@ ipset_all_ip(){
     ipset -q ${CREATE} ${PREFIX}IP${setCount} ${IPHASH}
     ipset -q ${DESTROY} ${PREFIX}.tmp
 
-  (echo "${CREATE} ${PREFIX}.tmp ${IPHASH}"
-  sed -n "/\//!p" ${BLOCKLIST_FILE} | sed -n "$(((( setCount - 1) * IPSET_MAX) + 1)),$(( setCount * IPSET_MAX )) s/^/$ADD ${PREFIX}.tmp /p"
-  echo "COMMIT") | nice -n 15 ipset ${RESTORE} && ipset ${SWAP} ${PREFIX}.tmp ${PREFIX}IP${setCount}
+    (echo "${CREATE} ${PREFIX}.tmp ${IPHASH}"
+    sed -n "/\//!p" ${BLOCKLIST_FILE} | \
+      sed -n "$(((( setCount - 1) * IPSET_MAX) + 1)),$(( setCount * IPSET_MAX )) s/^/$ADD ${PREFIX}.tmp /p"
+    echo "COMMIT") | nice -n 15 ipset ${RESTORE} && \
+      ipset ${SWAP} ${PREFIX}.tmp ${PREFIX}IP${setCount}
 
-  iptables -t raw -nL PREROUTING | grep -q ${PREFIX}IP${setCount} || iptables -t raw -I PREROUTING -m set ${MATCH_SET} ${PREFIX}IP${setCount} src -j ${DROP_TARGET}
+    iptables -t raw -nL ${DROP_TARGET} | grep -q ${PREFIX}IP${setCount} || \
+      iptables -t raw -I ${DROP_TARGET} -m set ${MATCH_SET} ${PREFIX}IP${setCount} src -j DROP
 
-  msg="$msg ${PREFIX}IP${setCount} (( $(ipset -L ${PREFIX}IP${setCount} | wc -l) - ${ESL}))"
-  setCount=$((setCount+1))
+    msg="$msg ${PREFIX}IP${setCount} (( $(ipset -L ${PREFIX}IP${setCount} | wc -l) - ${ESL}))"
+    setCount=$((setCount+1))
 
   done
 }
@@ -206,41 +240,27 @@ ipset_all_ip(){
 clean_iptables(){
   [ -t 1 ] && send_log "clean iptables"
 
-  for SRC in $(iptables -nL PREROUTING -t raw | sed -n "/match-set/ s/.* \(${PREFIX}.*\) .*/\1/p")
-do
-  send_log "removing ipset / iptables SRC for ${SRC}"
-  ipset -q "${SWAP}" "${SRC}" "${SRC}" && \
-    iptables -t raw \
-      -D PREROUTING \
-      -m set ${MATCH_SET} "${SRC}" src \
-      -j ${DROP_TARGET} && ipset ${DESTROY} "${SRC}"
-done
-}
-
-get_lists(){
-  [ -t 1 ] && send_log "get list"
-
-  ( (while read -r url
-  do
-    nice -n 15 curl "$url" -sL
-  done <${BLOCKLIST_URLS} ); [ -s ${BLOCKLIST_DENY} ] && cat ${BLOCKLIST_DENY}) | \
-    nice -n 15 sed -n "s/\r//;s/#.*$//;/^$/d;/^[0-9,\.,\/]*$/p" | \
-    nice -n 15 grep -vf ${BLOCKLIST_ALLOW} | \
-    nice -n 15 awk '!a[$0]++' > ${BLOCKLIST_FILE}
+  iptables -t raw -D ${JUMP}
+  
+  iptables -t raw -F "${DROP_TARGET}"
+  iptables -t raw -X "${DROP_TARGET}"
+  exit 0
 }
 
 cleanup(){
   [ -t 1 ] && send_log "cleanup"
   ipset ${DESTROY} ${PREFIX}.tmp
   rm ${BLOCKLIST_FILE}
+  rm ${TMP_DIR}
 }
 
 main(){
   send_log "Begin Processing"
 
-  get_lists
-
   clean_iptables
+
+  get_lists
+  get_countries
 
   ipset_all_ip
   ipset_all_cidr
@@ -250,10 +270,12 @@ main(){
   send_log "End Processing"
 }
 
-[ "$1" = "clean" ] && \
-  init_ipset && \
-  clean_iptables && \
+set -x
+if [ "$1" = "clean" ]; then
+  clean_iptables
+  load_ipset
   exit 0
+fi
 
 init || cleanup
 main
